8aaa407806827a208f088acdcd45eaff2dc57a4c
Luke Conibear <laconibear@gmail.com>
2022-04-13 12:41:51 +0100

    updating conda environment

--- docs/distributed/distributed_ml_on_arc4_cpu.bash
+++ docs/distributed/distributed_ml_on_arc4_cpu.bash
@@ -4,7 +4,7 @@
 #$ -pe smp 12
 #$ -l h_vmem=6G
 
-conda activate intro_ml
+conda activate swd8_intro_ml
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CONDA_PREFIX/lib  # (sometimes needed)
 
 python tensorflow_ray_train_mnist_example.py --num-workers 12 --epochs 100
--- docs/distributed/distributed_ml_on_arc4_gpu.bash
+++ docs/distributed/distributed_ml_on_arc4_gpu.bash
@@ -3,8 +3,8 @@
 #$ -l h_rt=00:05:00
 #$ -l coproc_v100=1
 
-conda activate intro_ml 
+conda activate swd8_intro_ml 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CONDA_PREFIX/lib  # (sometimes required)
 
-python tensorflow_ray_train_mnist_example.py --use-gpu=True --epochs 100
-# python pytorch_ray_train_fashion_mnist_example.py --use-gpu=True --epochs 100
+python tensorflow_ray_train_mnist_example.py --use-gpu True --num-workers 2 --epochs 100
+# python pytorch_ray_train_fashion_mnist_example.py --use-gpu True --num-workers 2 --epochs 100
